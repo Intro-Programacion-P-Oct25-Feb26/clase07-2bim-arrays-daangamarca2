aaa8dbce9530ae0f058778294f35c2e83af203d3
daangamarca2 <arturoangamarca120@gmail.com>
2026-01-22 11:13:36 -0500

    Problema-021

--- ManejoArrays/src/paquete01/ArrayList021.java
+++ ManejoArrays/src/paquete01/ArrayList021.java
@@ -16,65 +16,64 @@ import java.util.Scanner;
 public class ArrayList021 {
 
     public static void main(String[] args) {
-     
+
         ArrayList<String> arreglo2 = obtenerDatos();
         String cadenaFinal = obtenerCadenaFinal(arreglo2);
         ArrayList<String> iniciales = obtenerDatosIniciales(arreglo2);
-        String cadenaFinal2 = obtenerCadenaFinal(iniciales);
-        
+        ArrayList<String> inicialesMinusculas = convertirMinusculas(iniciales);
+        String cadenaFinal2 = obtenerCadenaFinal(inicialesMinusculas);
+
         System.out.printf("%s\n", cadenaFinal);
-        
         System.out.printf("%s\n", cadenaFinal2);
-        
     }
 
-    public static ArrayList<String> obtenerDatosIniciales(ArrayList<String> nombres){
+    public static ArrayList<String> obtenerDatosIniciales(ArrayList<String> nombres) {
         ArrayList<String> arreglo = new ArrayList<>();
-        String auxiliar;
         for (int i = 0; i < nombres.size(); i++) {
-            auxiliar = nombres.get(i);
+            String auxiliar = nombres.get(i);
             auxiliar = auxiliar.substring(0, 1);
             arreglo.add(auxiliar);
         }
-        
         return arreglo;
     }
-    
-    public static ArrayList<String> obtenerDatos(){
+
+    public static ArrayList<String> obtenerDatos() {
         ArrayList<String> arreglo2 = new ArrayList<>();
         boolean bandera = true;
-        String pais;
         Scanner entrada = new Scanner(System.in);
         String opcion;
+
         while (bandera) {
             System.out.println("Ingrese un nombre de un país");
-            pais = entrada.nextLine(); // Ecuador / ecuador / ECuador
-            pais = convertirMayusculas(pais);
-            arreglo2.add(pais);
+            String pais = entrada.nextLine();
+            arreglo2.add(convertirMayusculas(pais));
+
             System.out.println("Desea ingresar salir, ingrese (s)");
             opcion = entrada.nextLine();
-            if (opcion.equals("s")) {
+            if (opcion.equalsIgnoreCase("s")) {
                 bandera = false;
             }
         }
         return arreglo2;
-        
     }
-    
-    public static String convertirMayusculas(String c){
-        String miValor = c.toUpperCase();
-        return miValor;
+
+    public static String convertirMayusculas(String c) {
+        return c.toUpperCase();
     }
-        
-    public static String obtenerCadenaFinal(ArrayList<String> lista){
-        String cadenaFinal = "";
-        
+
+    public static ArrayList<String> convertirMinusculas(ArrayList<String> lista) {
+        ArrayList<String> resultado = new ArrayList<>();
         for (int i = 0; i < lista.size(); i++) {
-            cadenaFinal = String.format("%s%s\n", cadenaFinal,
-                    lista.get(i));
+            resultado.add(lista.get(i).toLowerCase());
+        }
+        return resultado;
+    }
+
+    public static String obtenerCadenaFinal(ArrayList<String> lista) {
+        String cadenaFinal = "";
+        for (int i = 0; i < lista.size(); i++) {
+            cadenaFinal = String.format("%s%s\n", cadenaFinal, lista.get(i));
         }
         return cadenaFinal;
-    }    
-        
-    
+    }
 }
